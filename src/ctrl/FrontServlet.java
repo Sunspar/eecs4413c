b3b83e18e31271c6f2ac56107507a74e812ca6c8
Andrew Turner <oraclesunspar@gmail.com>
2014-11-12 19:03:50 -0500

    Initial commit

---
+++ src/ctrl/FrontServlet.java
@@ -0,0 +1,72 @@
+package ctrl;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet(
+		name = "Front Servlet for Foods R Us",
+		urlPatterns = "/eFoods"
+)
+public class FrontServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FrontServlet() {
+        super();
+    }
+
+	/**
+	 * @see Servlet#init()
+	 */
+	public void init() throws ServletException {
+		super.init();
+		
+		//Load in the properties file
+		try {
+			Properties properties = new Properties();
+			String value = getServletContext().getInitParameter("SHARED_VALUES_FILE");
+			InputStream streamData = getServletContext().getResourceAsStream(value);
+			properties.load(streamData);
+			
+			getServletContext().setAttribute(
+					getServletContext().getInitParameter("PROPERTIES"),
+					properties	
+			);
+			
+		} catch (IOException e) {
+			throw new ServletException(e.getMessage(), e.getCause());
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ServletContext ctx = getServletContext();
+		HttpSession session = request.getSession();
+		String targetPage = ""; // WE DONT KNOW YET GOSH.
+		Properties props = (Properties) ctx.getAttribute(ctx.getInitParameter("PROPERTIES"));
+		String testVal = props.getProperty("FD_TEST_VAL");
+		
+		
+		
+	}
+
+}
