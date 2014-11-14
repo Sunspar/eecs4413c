0d2867719a214c36d2a9f8ed21612cde9d94568d
Andrew Turner <oraclesunspar@gmail.com>
2014-11-13 20:49:35 -0500

    Simple bean objects for the Category and Item tables.

---
+++ src/model/CategoryBean.java
@@ -0,0 +1,63 @@
+package model;
+
+import java.sql.Blob;
+
+public class CategoryBean {
+	public int id;
+	public String name;
+	public String description;
+	public byte[] picture;
+	
+	/**
+	 * Represents a Category of item that we sell.
+	 * 
+	 * @param id The id of the category -- a numeric value used to group items by category
+	 * @param name The name of the category
+	 * @param description A description about what this category holds
+	 * @param picture A picture that represents the category
+	 */
+	public CategoryBean(
+			int id,
+			String name,
+			String description,
+			byte[] picture) {
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.picture = picture;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public byte[] getPicture() {
+		return picture;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public void setPicture(byte[] picture) {
+		this.picture = picture;
+	}
+	
+	
+}
